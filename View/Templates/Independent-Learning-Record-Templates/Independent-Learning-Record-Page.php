6a4e09092132d0a1586435c03ef285dd8147a594
anjumun <samuroth@gmail.com>
2017-11-02 09:04:11 -0400

    added tabs to independant learning record page

--- View/Templates/Independent-Learning-Record-Templates/Independent-Learning-Record-Page.php
+++ View/Templates/Independent-Learning-Record-Templates/Independent-Learning-Record-Page.php
@@ -1,11 +1,13 @@
 <html>
     <head>
         <link rel="stylesheet" type="text/css" href="CSS/Main-Evaluation-Page-Style.css">
+        <link rel="stylesheet" type="text/css" href="CSS/ILRCSS.css">
         <title>ILR Form</title>
         <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
         <script src="JS/DisplayStudentProfilePicture.js"></script>
         <script src="JS/ShowOtherMajorField.js"></script>
         <script src="JS/RetrieveStudentInfoById.js"></script>
+
     </head>
     <body>
         <!-- top navigation -->
@@ -21,16 +23,24 @@
                 <h1>Chemistry Department</h1>
                 <h2>Undergraduate Individual Learning Record</h2>
         </header>
-        <h2 id="ILR-Title">Biographical Information </h2>
-        <h5 id="Current_Date"></h5>
-        <script src="JS/ShowCurrentDate.js"></script>
-        <!-- read in main form -->
-        <?php
-              include "View/Templates/HTMLmodules/ILRform.html";
-        ?>
-        <!-- END read in main form -->
 
+
+        <div class="tab">
+        <button class="tablinks" onclick="openTab(event, 'Student-Information-Form')">Biographical Information</button>
+        <button class="tablinks" onclick="openTab(event, 'AdmissionsPlacementInfo')">Admissions Placement</button>
+        <button class="tablinks" onclick="openTab(event, 'MiscNotes')">Miscelaneous Notes</button>
+        </div>
+
+        <!-- TABS -->
+        <?php
+        include "View/Templates/HTMLmodules/ILRStudent-Information-Form.html";
+        include "View/Templates/HTMLmodules/ILRAdmissionsPlacementInfo.html";
+        include "View/Templates/HTMLmodules/ILRMiscNotes.html";
+        ?>
+        <!-- END TABS -->
+        <script src="JS/openTabs.js"></script>
     <body>
+
 <html>
 <?php
     include "Controller/Independent-Learning-Record-Controllers/Independent-Learning-Record-Student-Data-Input.php";
