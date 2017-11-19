93e98723dbdfef894e35bfa3d12fbc8541849bcf
j-nicholson <johnfnicholson123@gmail.com>
2017-11-19 16:28:55 -0500

    Misc Student Notes are now being displayed in table format

--- Controller/Independent-Learning-Record-Controllers/Independent-Learning-Record-Student-Retrieval.php
+++ Controller/Independent-Learning-Record-Controllers/Independent-Learning-Record-Student-Retrieval.php
@@ -3,14 +3,11 @@
 
     include "../DBConnection.php";
 
-    $query = "SELECT Student_ID, Student_Name, Student_Major, Student_Year, Student_Semester, Student_Date, Student_Photo, Student_Math_Grade, Student_Athletics, Student_Housing_Status, Student_Honors, International_Student, Misc_Text FROM Student INNER JOIN Misc_Notes ON Misc_Notes.Misc_Student_ID = Student.Student_ID WHERE Student_ID = '" . $q . "'";
+    $query = "SELECT * FROM Student WHERE Student_ID = '" . $q . "'";
     $result = mysqli_query($connection, $query);
-
-    
 
     while($row = mysqli_fetch_assoc($result)) {
         echo json_encode($row);
     }
     mysqli_close($connection);
-
 ?>
