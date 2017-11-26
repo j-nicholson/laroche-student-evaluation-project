eb2cd9a9e6a28a742be181205b23b46e2591718f
j-nicholson <johnfnicholson123@gmail.com>
2017-11-25 20:18:49 -0500

    Updated SQL Database and organized misc notes posts by date

--- SQL/MainDatabase.sql
+++ SQL/MainDatabase.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Nov 17, 2017 at 05:32 PM
+-- Generation Time: Nov 26, 2017 at 02:14 AM
 -- Server version: 10.1.26-MariaDB
 -- PHP Version: 7.1.8
 
@@ -101,7 +101,8 @@ CREATE TABLE IF NOT EXISTS `Course_Assessment_Form_Section_1` (
 INSERT INTO `Course_Assessment_Form_Section_1` (`CAFS1ID`, `NewSLOs`, `Upgrades`, `Enhancements`, `Modifications`, `Other`) VALUES
 ('CHEM2016-SPR2017', 0x37363538373635383736, 0x3736383736383937363839, 0x3738363938373639383736, 0x37363938373639383736, 0x38373039383736303837363837),
 ('CSCI4098-SPR2017', 0x6666647361, 0x66647361, 0x66647361, 0x66647361, 0x66647361),
-('CSCI4098-SPR2017', '', '', '', '', '');
+('CSCI4098-SPR2017', '', '', '', '', ''),
+('MATH2050-SPR2017', '', '', '', '', '');
 
 -- --------------------------------------------------------
 
@@ -129,6 +130,7 @@ INSERT INTO `Course_Assessment_Form_Section_2` (`ChemConcept`, `ObjectivesConten
 (0x383039373630393837363938, 0x303938373637363738363839, 0x383739303736383736393837, '1', '1', '1', '1', '1'),
 (0x6664, 0x7666647361, 0x66647361, '1', '1', '1', '1', '1'),
 (0x6664, 0x6664666461, 0x66647361, '1', '2', '', '2', ''),
+('', '', '', '', '', '', '', ''),
 ('', '', '', '', '', '', '', '');
 
 -- --------------------------------------------------------
@@ -156,7 +158,39 @@ INSERT INTO `Course_Assessment_Form_Section_3` (`CoursePreReq`, `Textbooks`, `St
 (0x343336353334353633343536, 0x34353634353633343536, 0x34353336343536333435, 0x343536333435363435, 0x333435363435363435, 0x343335363435363334, 0x343536333435363334),
 (0x66646173, 0x66646173, 0x66646173, 0x66647361, 0x66646173, '', ''),
 (0x66646173, 0x66647361, 0x66647361, 0x66647361, '', 0x66646173, 0x66647361),
+('', '', '', '', '', '', ''),
 ('', '', '', '', '', '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Course_Assessment_Form_Section_4`
+--
+
+DROP TABLE IF EXISTS `Course_Assessment_Form_Section_4`;
+CREATE TABLE IF NOT EXISTS `Course_Assessment_Form_Section_4` (
+  `Sec4Num` int(20) NOT NULL,
+  `IDNum` int(20) NOT NULL,
+  `LearningIssue` varchar(255) NOT NULL,
+  `ExtentInstructor` longblob NOT NULL,
+  `InstructorRecommendations` longblob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Course_Assessment_Form_Section_5`
+--
+
+DROP TABLE IF EXISTS `Course_Assessment_Form_Section_5`;
+CREATE TABLE IF NOT EXISTS `Course_Assessment_Form_Section_5` (
+  `Sec5Num` int(20) NOT NULL,
+  `IDNum` int(20) NOT NULL,
+  `Grade` varchar(1) NOT NULL,
+  `LearningIssue` varchar(255) NOT NULL,
+  `ExtentInstructorInt` longblob NOT NULL,
+  `InstructorEvalRecommendations` longblob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -191,6 +225,7 @@ INSERT INTO `Course_SLOs` (`CourseSLOID`, `CourseID`, `SLO1`, `SLO2`, `SLO3`, `S
 (608109, 'CSCI4098', 'false', 'true', 'true', 'false', 'false'),
 (621863, 'CSCI4098', 'false', 'true', 'true', 'false', 'false'),
 (649302, 'CSCI4098', 'false', 'true', 'true', 'false', 'false'),
+(705848, 'MATH2050', 'false', 'true', 'true', 'false', 'false'),
 (772739, 'MATH2050', 'true', 'true', 'false', 'false', 'false'),
 (835938, 'CSCI4098', 'false', 'true', 'true', 'false', 'false'),
 (837367, '', 'true', 'true', 'false', 'false', 'false'),
@@ -229,23 +264,30 @@ INSERT INTO `Instructor` (`InstructorID`, `InstructorName`, `InstructorTitle`) V
 DROP TABLE IF EXISTS `Misc_Notes`;
 CREATE TABLE IF NOT EXISTS `Misc_Notes` (
   `Misc_ID` int(11) NOT NULL AUTO_INCREMENT,
-  `Misc_Title` varchar(255) DEFAULT NULL,
   `Misc_Text` text NOT NULL,
   `Misc_Date` datetime NOT NULL,
   `Misc_Student_ID` int(11) DEFAULT NULL,
   `Misc_Author_ID` int(11) DEFAULT NULL,
   PRIMARY KEY (`Misc_ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `Misc_Notes`
 --
 
-INSERT INTO `Misc_Notes` (`Misc_ID`, `Misc_Title`, `Misc_Text`, `Misc_Date`, `Misc_Student_ID`, `Misc_Author_ID`) VALUES
-(1, NULL, 'fsafdsafasdf', '2017-11-14 23:22:45', 123456, NULL),
-(2, NULL, 'fdsafasdf', '2017-11-15 15:36:31', 123456, NULL),
-(3, NULL, 'Some notes about this student ID', '2017-11-17 15:47:56', 654321, NULL),
-(4, NULL, 'hey there', '2017-11-17 16:15:06', 131313, NULL);
+INSERT INTO `Misc_Notes` (`Misc_ID`, `Misc_Text`, `Misc_Date`, `Misc_Student_ID`, `Misc_Author_ID`) VALUES
+(1, 'fsafdsafasdf', '2017-11-14 23:22:45', 123456, NULL),
+(2, 'fdsafasdf', '2017-11-15 15:36:31', 123456, NULL),
+(3, 'Some notes about this student ID', '2017-11-17 15:47:56', 654321, NULL),
+(4, 'hey there', '2017-11-17 16:15:06', 131313, NULL),
+(5, 'fsa', '2017-11-19 18:48:55', 123456, NULL),
+(6, 'A new note has been added', '2017-11-19 22:11:23', 123456, NULL),
+(7, 'New note', '2017-11-19 22:15:58', 654321, NULL),
+(8, 'latest note', '2017-11-19 22:16:26', 654321, NULL),
+(9, 'New entry', '2017-11-20 21:36:49', 123456, NULL),
+(10, 'Another new entry', '2017-11-20 21:37:12', 123456, NULL),
+(11, 'New stuff', '2017-11-20 21:38:32', 123456, NULL),
+(12, 'This note was added with the most recent post at the top of the list.', '2017-11-26 02:04:39', 123456, NULL);
 
 -- --------------------------------------------------------
 
@@ -287,7 +329,6 @@ CREATE TABLE IF NOT EXISTS `Student` (
   `Student_Semester` varchar(32) NOT NULL,
   `Student_Date` datetime DEFAULT NULL,
   `Student_Photo` varchar(255) DEFAULT NULL,
-  `Student_Notes` longtext NOT NULL,
   `Student_Math_Grade` varchar(10) DEFAULT NULL,
   `Student_Athletics` varchar(255) DEFAULT NULL,
   `Student_Housing_Status` varchar(255) DEFAULT NULL,
@@ -300,14 +341,14 @@ CREATE TABLE IF NOT EXISTS `Student` (
 -- Dumping data for table `Student`
 --
 
-INSERT INTO `Student` (`Student_ID`, `Student_Name`, `Student_Major`, `Student_Year`, `Student_Semester`, `Student_Date`, `Student_Photo`, `Student_Notes`, `Student_Math_Grade`, `Student_Athletics`, `Student_Housing_Status`, `Student_Honors`, `International_Student`) VALUES
-(123456, 'Johnny Nicholson', 'Computer_Science', '2017', 'Spring', '2017-11-15 15:35:14', '', '', 'A', 'no', 'resident', 'No', 'No'),
-(323232, 'New Tester', 'Chemistry', '2014', 'Fall', '2017-11-14 22:49:48', '', '', 'A', 'fdsf', 'commuter', 'No', 'No'),
-(555555, 'New Guy', 'Chemistry', '2014', 'Fall', '2017-11-14 20:27:00', '', '', 'A', 'no', 'commuter', 'No', 'No'),
-(654321, 'Andrew Leach', 'Computer_Science', '2017', 'Summer', '2017-11-17 16:08:49', '', '', 'B-', 'Cross Country', 'resident', 'No', 'Japan'),
-(666666, 'New Guy', 'Chemistry', '2014', 'Fall', '2017-11-14 20:28:36', '', '', 'A', 'no', 'commuter', 'No', 'No'),
-(777777, 'TEST', 'Chemistry', '2014', 'Fall', '2017-11-14 20:46:10', '', '', 'A', 'Football', 'commuter', 'No', 'Tiwan'),
-(989898, 'John Nich', 'Chemistry', '2014', 'Fall', '2017-11-15 17:20:29', '', '', 'B-', 'no', 'resident', 'No', 'No');
+INSERT INTO `Student` (`Student_ID`, `Student_Name`, `Student_Major`, `Student_Year`, `Student_Semester`, `Student_Date`, `Student_Photo`, `Student_Math_Grade`, `Student_Athletics`, `Student_Housing_Status`, `Student_Honors`, `International_Student`) VALUES
+(123456, 'Johnny Nicholson', 'Computer_Science', '2017', 'Spring', '2017-11-15 15:35:14', '', 'A', 'no', 'resident', 'No', 'No'),
+(323232, 'New Tester', 'Chemistry', '2014', 'Fall', '2017-11-14 22:49:48', '', 'A', 'fdsf', 'commuter', 'No', 'No'),
+(555555, 'New Guy', 'Chemistry', '2014', 'Fall', '2017-11-14 20:27:00', '', 'A', 'no', 'commuter', 'No', 'No'),
+(654321, 'Andrew Leach', 'Computer_Science', '2017', 'Summer', '2017-11-17 16:08:49', '', 'B-', 'Cross Country', 'resident', 'No', 'Japan'),
+(666666, 'New Guy', 'Chemistry', '2014', 'Fall', '2017-11-14 20:28:36', '', 'A', 'no', 'commuter', 'No', 'No'),
+(777777, 'TEST', 'Chemistry', '2014', 'Fall', '2017-11-14 20:46:10', '', 'A', 'Football', 'commuter', 'No', 'Tiwan'),
+(989898, 'John Nich', 'Chemistry', '2014', 'Fall', '2017-11-15 17:20:29', '', 'B-', 'no', 'resident', 'No', 'No');
 
 -- --------------------------------------------------------
 
@@ -321,7 +362,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
