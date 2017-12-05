d2fd0dd2c3724ecba4ef08035ea6e8fb0d430306
j-nicholson <johnfnicholson123@gmail.com>
2017-12-04 20:18:04 -0500

    Started implementing feautres to keep context upon page reload. Having issues with certain situations

--- Controller/Independent-Learning-Record-Controllers/Independent-Learning-Record-Student-Data-Input.php
+++ Controller/Independent-Learning-Record-Controllers/Independent-Learning-Record-Student-Data-Input.php
@@ -3,21 +3,24 @@
     include "Controller/Independent-Learning-Record-Controllers/Independent-Learning-Record-Data-Handler.php";
     include "Controller/DBConnection.php";
     $student = new Student();
-    //$globalStudentID = 0;
+
     if($_GET['action'] == 'submitStudentInfo') {
       if(isset($_POST['submit'])) {
         if($_SERVER['REQUEST_METHOD'] == "POST") {
-
+            // Set the student Date
             $student->set_student_date(date('Y-m-d H:i:s'));
 
+            // Set the student ID
             $secure_id = DataHandler::secure_input($_POST['studentId']);
             $student->set_student_ID($secure_id);
-            //super global is needed as the contructor for MiscNotes requires a valid studentID
-            //$GLOBALS['globalStudentID'] = $secure_id;
+            $_SESSION['studentId'] = $student->get_student_ID();
 
+            // Set the student Name
             $secure_name = DataHandler::secure_input($_POST['studentName']);
             $student->set_student_name($secure_name);
+            $_SESSION['studentName'] = $student->get_student_name();
 
+            // Set the student Photo
             if(is_uploaded_file($_FILES['image']['tmp_name'])) {
                 $basefolder = "Controller/Independent-Learning-Record-Controllers/Student-Photos/" . $student->get_student_name() . "/";
                 $studentImage = $_FILES['image']['name'];
@@ -43,12 +46,15 @@
                 }
             }
 
+            // Set the student Major
             switch($_POST['major']) {
                 case $_POST['major'] == "Chemistry":
                     $student->set_student_major("Chemistry");
+                    $_SESSION['studentMajor'] = $student->get_student_major();
                     break;
                 case $_POST['major'] == "Biochemistry":
                     $student->set_student_major("Biochemistry");
+                    $_SESSION['studentMajor'] = $student->get_student_major();
                     break;
                 case $_POST['major'] == "OtherOption":
                     $secure_major = DataHandler::secure_input($_POST['Other']);
@@ -56,120 +62,147 @@
                     if($position !== false){
                         $secure_major = str_replace(" ", "_", $secure_major);
                         $student->set_student_major($secure_major);
+                        $_SESSION['studentMajor'] = $student->get_student_major();
                     } else {
                         $student->set_student_major($secure_major);
+                        $_SESSION['studentMajor'] = $student->get_student_major();
                     }
                     break;
                 default:
                     break;
             }
 
+            // Set the student Year
             $student->set_student_year($_POST['year']);
+            $_SESSION['studentYear'] = $student->get_student_year();
 
+            // Set the student Semester
             switch($_POST['semester']) {
                 case $_POST['semester'] == "Fall":
                     $student->set_student_semester("Fall");
+                    $_SESSION['studentSemester'] = $student->get_student_semester();
                     break;
                 case $_POST['semester'] == "Spring":
                     $student->set_student_semester("Spring");
+                    $_SESSION['studentSemester'] = $student->get_student_semester();
                     break;
                 case $_POST['semester'] == "Summer":
                     $student->set_student_semester("Summer");
+                    $_SESSION['studentSemester'] = $student->get_student_semester();
                     break;
                 default:
                     break;
             }
 
-            //MathPlacement Input to the Database
+            // Set the student math placement results
             switch($_POST['mathPlacement']){
                 case $_POST['mathPlacement'] == "A":
                     $student->set_student_math_grade("A");
+                    $_SESSION['studentMathPlacement'] = $student->get_student_math_grade();
                     break;
                 case $_POST['mathPlacement'] == "A-":
                     $student->set_student_math_grade("A-");
+                    $_SESSION['studentMathPlacement'] = $student->get_student_math_grade();
                     break;
                 case $_POST['mathPlacement'] == "B+":
                     $student->set_student_math_grade("B+");
+                    $_SESSION['studentMathPlacement'] = $student->get_student_math_grade();
                     break;
                 case $_POST['mathPlacement'] == "B":
                     $student->set_student_math_grade("B");
+                    $_SESSION['studentMathPlacement'] = $student->get_student_math_grade();
                     break;
                 case $_POST['mathPlacement'] == "B-":
                     $student->set_student_math_grade("B-");
+                    $_SESSION['studentMathPlacement'] = $student->get_student_math_grade();
                     break;
                 case $_POST['mathPlacement'] == "C+":
                     $student->set_student_math_grade("C+");
+                    $_SESSION['studentMathPlacement'] = $student->get_student_math_grade();
                     break;
                 case $_POST['mathPlacement'] == "C":
                     $student->set_student_math_grade("C");
+                    $_SESSION['studentMathPlacement'] = $student->get_student_math_grade();
                     break;
                 case $_POST['mathPlacement'] == "C-":
                     $student->set_student_math_grade("C-");
+                    $_SESSION['studentMathPlacement'] = $student->get_student_math_grade();
                     break;
                 case $_POST['mathPlacement'] == "D+":
                     $student->set_student_math_grade("D+");
+                    $_SESSION['studentMathPlacement'] = $student->get_student_math_grade();
                     break;
                 case $_POST['mathPlacement'] == "D":
                     $student->set_student_math_grade("D");
+                    $_SESSION['studentMathPlacement'] = $student->get_student_math_grade();
                     break;
                 case $_POST['mathPlacement'] == "D-":
                     $student->set_student_math_grade("D-");
+                    $_SESSION['studentMathPlacement'] = $student->get_student_math_grade();
                     break;
                 case $_POST['mathPlacement'] == "F":
                     $student->set_student_math_grade("F");
+                    $_SESSION['studentMathPlacement'] = $student->get_student_math_grade();
                     break;
                 default:
                     break;
             }
 
-            //Athletics Input to the Database
+            // Set the student athletics status
             switch ($_POST['athletes']){
                 case $_POST['athletes'] == "no":
                     $student->set_student_Athletics("no");
+                    $_SESSION['studentAthletes'] = $student->get_student_Athletics();
                     break;
                 case $_POST['athletes'] == "yes":
-                     $secure_Athlete_Student = DataHandler::secure_input($_POST['AthleticsDisplay']);
+                    $secure_Athlete_Student = DataHandler::secure_input($_POST['AthleticsDisplay']);
                     $position = strpos($secure_Athlete_Student, " ");
                     if($position !== false){
                         $secure_major = str_replace(" ", "_", $secure_Athlete_Student);
                         $student->set_student_Athletics($secure_Athlete_Student);
+                        $_SESSION['studentAthletes'] = $student->get_student_Athletics();
                     } else {
                         $student->set_student_Athletics($secure_Athlete_Student);
+                        $_SESSION['studentAthletes'] = $student->get_student_Athletics();
                     }
                     break;
-
                 default:
                     break;
             }
+
+            // Set the student housing status
              switch($_POST['housing']){
                 case $_POST['housing'] == "resident":
                     $student->set_student_Housing_Status("resident");
+                    $_SESSION['studentHousing'] = $student->get_student_Housing_Status();
                     break;
                 case $_POST['housing']  == "commuter":
                     $student->set_student_Housing_Status("commuter");
+                    $_SESSION['studentHousing'] = $student->get_student_Housing_Status();
                     break;
                 default:
                     break;
             }
 
-            //Student honors Intitute Input to the Database
-
+            // Set the student honors status
             switch($_POST['honors']){
                 case $_POST['honors'] == "honorsYes":
                     $student->set_student_Honors_Institute("Yes");
+                    $_SESSION['studentHonors'] = $student->get_student_Honors_Institute();
                     break;
                 case $_POST['honors'] == "honorsNo":
                     $student->set_student_Honors_Institute("No");
+                    $_SESSION['studentHonors'] = $student->get_student_Honors_Institute();
                     break;
                 default:
                     break;
             }
 
-            //International Student
-
+            // Set the student international status
             switch($_POST['InternationalStudent']){
                 case $_POST['InternationalStudent'] == "No":
                     $student->set_student_International_Student("No");
+                    $_SESSION['InternationalStudent'] = $student->get_student_International_Student();
                     break;
                 case $_POST['InternationalStudent'] == "Yes":
                     $secure_Int_Student = DataHandler::secure_input($_POST['OtherInternational']);
@@ -177,34 +210,18 @@
                     if($position !== false){
                         $secure_major = str_replace(" ", "_", $secure_Int_Student);
                         $student->set_student_International_Student($secure_Int_Student);
+                        $_SESSION['InternationalStudent'] = $student->get_student_International_Student();
                     } else {
                         $student->set_student_International_Student($secure_Int_Student);
+                        $_SESSION['InternationalStudent'] = $student->get_student_International_Student();
                     }
                 default:
                     break;
 
             }
-            Datahandler::insert_student($student->get_student_ID(), $student->get_student_name(), $student->get_student_major(), $student->get_student_year(), $student->get_student_semester(), $student->get_student_photo(), $student->get_student_date(), $student->get_student_math_grade(), $student->get_student_Athletics(), $student-> get_student_Housing_Status(),$student->get_student_Honors_Institute(),$student->get_student_International_Student());
+            // Insert student info to database
+            Datahandler::insert_student($_SESSION['studentId'], $_SESSION['studentName'], $_SESSION['studentMajor'], $_SESSION['studentYear'], $_SESSION['studentSemester'], $student->get_student_photo(), $student->get_student_date(), $_SESSION['studentMathPlacement'], $_SESSION['studentAthletes'], $_SESSION['studentHousing'], $_SESSION['studentHonors'], $_SESSION['InternationalStudent']);
         }
       }
     }
-    //MiscNotes object is made here with the super global $globalStudentID
-    /*$miscNotes = new MiscNotes($globalStudentID);
-
-    if(isset($_POST['submitNotes'])) {
-        if($_SERVER['REQUEST_METHOD'] == "POST") {
-            echo $globalStudentID;
-            $secure_notes = DataHandler::secure_input($_POST['notes']);
-            $miscNotes->set_student_misc_notes($secure_notes);
-
-            $last_time_edited = $_POST['lastTimeEdited'];
-
-            $miscNotes->set_last_time_edited($last_time_edited);
-
-            $userEditing = $_POST['userLoggedIn'];
-            $miscNotes->set_user_editing($userEditing);
-
-            Datahandler::insert_notes($miscNotes->get_studentID(),$miscNotes->get_misc_notes(), $miscNotes->get_user_editing_info(), $miscNotes->get_last_time_edited());
-        }
-    }*/
 ?>
