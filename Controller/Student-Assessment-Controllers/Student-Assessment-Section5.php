0c1571461244b1a739873975a9a28cbc094641f1
PontiacBrad <bradley.gray@stu.laroche.edu>
2017-12-06 15:48:32 -0500

    Fixed issue with IDNum and learning issues not saving into the database

--- Controller/Student-Assessment-Controllers/Student-Assessment-Section5.php
+++ Controller/Student-Assessment-Controllers/Student-Assessment-Section5.php
@@ -19,41 +19,41 @@
 		<tr>
 	        <tr>
 			<td><input type="text" size = "5" NAME="Section5Num[]" /></td>
-			<td><input type="text" size = "5" NAME="IDNum[]" /></td>
+			<td><input type="text" size = "5" NAME="S5IDNum[]" /></td>
             <td><input type="text" id="fname0" onchange="updateGrades('fname0')" size = "5" NAME="Grade[]" /></td>
-			<td><input type="text" size = "13" NAME="LearningIssue[]" /></td>
+			<td><input type="text" size = "13" NAME="S5LearningIssue[]" /></td>
 			<td><textarea NAME="ExtentInstructorInt[]" style="width: 350px; height: 75px"></textarea></td>
 			<td><textarea NAME="InstructorEvalRecommendations[]" style="width: 350px; height: 75px"></textarea></td>
 			</tr>
             <tr>
 			<td><input type="text" size = "5" NAME="Section5Num[]" /></td>
-			<td><input type="text" size = "5" NAME="IDNum[]" /></td>
+			<td><input type="text" size = "5" NAME="S5IDNum[]" /></td>
             <td><input type="text" id="fname1" onchange="updateGrades('fname1')" size = "5" NAME="Grade[]" /></td>
-			<td><input type="text" size = "13" NAME="LearningIssue[]" /></td>
+			<td><input type="text" size = "13" NAME="S5LearningIssue[]" /></td>
 			<td><textarea NAME="ExtentInstructorInt[]" style="width: 350px; height: 75px"></textarea></td>
 			<td><textarea NAME="InstructorEvalRecommendations[]" style="width: 350px; height: 75px"></textarea></td>
 			</tr>
             <tr>
             <td><input type="text" size = "5" NAME="Section5Num[]" /></td>
-			<td><input type="text" size = "5" NAME="IDNum[]" /></td>
+			<td><input type="text" size = "5" NAME="S5IDNum[]" /></td>
             <td><input type="text" id="fname2" onchange="updateGrades('fname2')" size = "5" NAME="Grade[]" /></td>
-			<td><input type="text" size = "13" NAME="LearningIssue[]" /></td>
+			<td><input type="text" size = "13" NAME="S5LearningIssue[]" /></td>
 			<td><textarea NAME="ExtentInstructorInt[]" style="width: 350px; height: 75px"></textarea></td>
 			<td><textarea NAME="InstructorEvalRecommendations[]" style="width: 350px; height: 75px"></textarea></td>
 			</tr>
              <tr>
            <td><input type="text" size = "5" NAME="Section5Num[]" /></td>
-			<td><input type="text" size = "5" NAME="IDNum[]" /></td>
+			<td><input type="text" size = "5" NAME="S5IDNum[]" /></td>
             <td><input type="text" id="fname3" onchange="updateGrades('fname3')" size = "5" NAME="Grade[]" /></td>
-			<td><input type="text" size = "13" NAME="LearningIssue[]" /></td>
+			<td><input type="text" size = "13" NAME="S5LearningIssue[]" /></td>
 			<td><textarea NAME="ExtentInstructorInt[]" style="width: 350px; height: 75px"></textarea></td>
 			<td><textarea NAME="InstructorEvalRecommendations[]" style="width: 350px; height: 75px"></textarea></td>
 			</tr>
             <tr>
             <td><input type="text" size = "5" NAME="Section5Num[]" /></td>
-			<td><input type="text" size = "5" NAME="IDNum[]" /></td>
+			<td><input type="text" size = "5" NAME="S5IDNum[]" /></td>
             <td><input type="text" id="fname4" onchange="updateGrades('fname4')" size = "5" NAME="Grade[]" /></td>
-			<td><input type="text" size = "13" NAME="LearningIssue[]" /></td>
+			<td><input type="text" size = "13" NAME="S5LearningIssue[]" /></td>
 			<td><textarea NAME="ExtentInstructorInt[]" style="width: 350px; height: 75px"></textarea></td>
 			<td><textarea NAME="InstructorEvalRecommendations[]" style="width: 350px; height: 75px"></textarea></td>
 			</tr>
--- Controller/Student-Assessment-Controllers/Student-Assessment-Update-Section5.php
+++ Controller/Student-Assessment-Controllers/Student-Assessment-Update-Section5.php
@@ -1,9 +1,8 @@
 <?php
     if (isset($_POST['submitted2'])){    
     include "Controller/DBConnection.php";
-        
         foreach($_POST['Section5Num'] as $idx => $Sec5Num) {
-    $sqlinsert="INSERT INTO Course_Assessment_Form_Section_5 (Sec5Num, IDNum, Grade, LearningIssue, ExtentInstructorInt, InstructorEvalRecommendations) VALUES ('" . $Sec5Num . "', '" . $_POST['IDNum'][$idx] . "', '" . $_POST['Grade'][$idx] . "', '" . $_POST['LearningIssue'][$idx] . "', '" . $_POST['ExtentInstructorInt'][$idx] . "','" . $_POST['InstructorEvalRecommendations'][$idx] . "' )";
+    $sqlinsert="INSERT INTO Course_Assessment_Form_Section_5 (Sec5Num, IDNum, Grade, LearningIssue, ExtentInstructorInt, InstructorEvalRecommendations) VALUES ('" . $Sec5Num . "', '" . $_POST['S5IDNum'][$idx] . "', '" . $_POST['Grade'][$idx] . "', '" . $_POST['S5LearningIssue'][$idx] . "', '" . $_POST['ExtentInstructorInt'][$idx] . "','" . $_POST['InstructorEvalRecommendations'][$idx] . "' )";
     
         
        
