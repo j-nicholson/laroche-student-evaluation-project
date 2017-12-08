719152b1097c8e98b7fbbfa1dc00e15cd23c5fd5
j-nicholson <johnfnicholson123@gmail.com>
2017-12-08 00:33:12 -0500

    Added support to radio buttons and Misc Notes tab to keep data upon page reload. Still working on picture bugs.

--- JS/studentAthleticsDisplay.js
+++ JS/studentAthleticsDisplay.js
@@ -1,6 +1,6 @@
 function displayStudentAthletics(id) {
   if (id === "yes") {
-    document.getElementById('StudentAthleticsDisplay').innerHTML = 'If yes, your sport name: <input id="AthleticsDisplay" name="AthleticsDisplay" type="text">';
+    document.getElementById('StudentAthleticsDisplay').innerHTML = 'If yes, your sport name: <input id="AthleticsDisplay" name="AthleticsDisplay" type="text" required>';
   }
   else {
     document.getElementById('StudentAthleticsDisplay').innerHTML = '';
